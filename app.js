156b966d8ec0eff9a9d4aa0d63760acd719d455c
Karan Singh <urskaransingh4@gmail.com>
2024-02-25 18:09:31 +0530

    webpage : modified
    HTML ,CSS modified and JS added

---
+++ app.js
@@ -0,0 +1,5 @@
+var fileInput = document.createElement('input');
+fileInput.setAttribute('type', 'file');
+var fileInputDiv = document.querySelector(".primary-button");
+fileInputDiv.appendChild(fileInput);
+
