8572db94fdc636b7d356bbdedc4d2c6789ef5fec
Karan Singh <urskaransingh4@gmail.com>
2024-03-02 23:52:34 +0530

    app.js:  added

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
-var fileInput = document.createElement('input');
-fileInput.setAttribute('type', 'file');
-var fileInputDiv = document.querySelector(".primary-button");
-fileInputDiv.appendChild(fileInput);
+const fileSelector = document.querySelector('#start');
+const start = document.querySelector('.btn');
+const img = document.querySelector('#img')
+const progress = document.querySelector('.progress');
+const textarea = document.querySelector('textarea');
 
+fileSelector.onchange = () => {
+    var file = fileSelector.files[0];
+    var imgUrl = window.URL.createObjectURL(new Blob([file],{type: 'image/jpg'}))
+    img.src = imgUrl
+}
+
+start.onclick = () => {
+    textarea.innerHTML = ''
+    const rec = new Tesseract.TesseractWorker()
+    rec.recognize(fileSelector.files[0])
+    .progress(function(response){
+        if(response.status == 'recognizing text'){
+            progress.innerHTML = response.status + ' '+response.progress
+        }
+        else {
+            progress.innerHTML = response.status
+        }
+    })
+    .then(function(data){
+        textarea.innerHTML = data.text
+        progress.innerHTML = 'Done'
+    }) 
+}
+
+
+
+
+
+
+// for animation purpose
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
